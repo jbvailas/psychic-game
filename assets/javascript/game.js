f2836bbd675c3eb8eb18a67952d7a86998e7d219
jbvailas <jbvailas@gmail.com>
2019-02-20 10:57:05 -0500

    rev-4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-//Global Variables//
+//Global Variables
 //======================================================================================
 
 //Arrays and Variables for holding data
@@ -9,26 +9,85 @@ var numBlanks = 0;
 var blankAndSuccesses = [];
 var wrongLetters = [];
 
-//Game Counters//
+//Game Counters
 var winCount = 0;
 var lossCount = 0;
 var guessesLeft = 9;
 
-//FUNCTIONS//
+//Functions
 //======================================================================================
 function startGame () {
     selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
     lettersinWord = selectedWord.split("");
     numBlanks = lettersinWord.length;
 
-//Reset//
+//Reset
 guessesLeft = 9;
 wrongLetters = [];
 blanksAndSuccesses = [];
+
+//Populate blanks and successes with right number of blanks
+for (var i=0; i<numBlanks; i++){
+    blanksAndSuccesses.push("_");
+}
+
+//Change HTML to reflect round condition
+document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+document.getElementById("numGuesses").innerHTML = guessesLeft;
+document.getElementById("winCounter").innerHTML = winCount;
+document.getElementById("lossCounter").innerHTML = lossCount;
 
 //Testing
 console.log(selectedWord);
 console.log(lettersinWord);
 console.log(numBlanks);
+console.log(blanksAndSuccesses);
+
+}
+
+function checkLetters(letter) {
+    //Check
+
+    var isLetterInWord = false;
+
+    for (var i=0; i<numBlanks; i++){
+        if(selectedWord[i] == letter) {
+            isLetterInWord = true;
+        }
+    }
+
+    if(isLetterinWOrd){
+    for (var i=0; i<numBlanks; i++) {
+        if(selectedWord[i] == letter) {
+                blanksAndSuccesses[i] == letter;
+            }
+        }
+
+    }
+
+    else {
+        wrongLetters.push(letter);
+        numGuesses--
+    }
+
+
+    console.log(blanksAndSuccesses);
+
+
+}
+
+
+
+//Main Process
+//=======================================================================================
+
+startGame();
+
+document.onkeyup = function(event) {
+   var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+   checkLetters(letterGuessed);
+
+   //Testing
+   alert(letterGuessed);
 
 }
