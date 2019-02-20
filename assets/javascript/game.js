3388f6b450dbde4292000599960451f9e598dee3
jbvailas <jbvailas@gmail.com>
2019-02-20 14:20:13 -0500

    rev-5

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,11 @@
-//Global Variables
-//======================================================================================
+//Psuedo Code --- To further develop this I would make the "_" be replaced with the actual letter so that you could see what letters you have left.
 
 //Arrays and Variables for holding data
-var wordOptions = ["brown", "green", "blue", "indigo", "black", "white", "yellow", "orange", "purple", "teal", "red"];
+var wordOptions = ["brown", "green", "blue", "indigo", "black", "white", "yellow", "orange", "purple", "red"];
 var selectedWord = "";
 var lettersinWord = [];
 var numBlanks = 0;
-var blankAndSuccesses = [];
+var blanksAndSuccesses = [];
 var wrongLetters = [];
 
 //Game Counters
@@ -22,14 +21,14 @@ function startGame () {
     numBlanks = lettersinWord.length;
 
 //Reset
-guessesLeft = 9;
-wrongLetters = [];
-blanksAndSuccesses = [];
+    guessesLeft = 9;
+    wrongLetters = [];
+    blanksAndSuccesses = [];
 
 //Populate blanks and successes with right number of blanks
-for (var i=0; i<numBlanks; i++){
-    blanksAndSuccesses.push("_");
-}
+    for (var i=0; i<numBlanks; i++) {
+        blanksAndSuccesses.push("_");
+    }
 
 //Change HTML to reflect round condition
 document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
@@ -56,9 +55,9 @@ function checkLetters(letter) {
         }
     }
 
-    if(isLetterinWOrd){
-    for (var i=0; i<numBlanks; i++) {
-        if(selectedWord[i] == letter) {
+    if(isLetterInWord) {
+        for (var i=0; i<numBlanks; i++) {
+            if(selectedWord[i] == letter) {
                 blanksAndSuccesses[i] == letter;
             }
         }
@@ -76,13 +75,13 @@ function checkLetters(letter) {
 }
 
 function roundComplete(){
-    console.log("Win Count: " + winCount + " | Loss Count: " + lossCount + " | Guesses Left" + numGuesses);
+    console.log("Win Count: " + winCount + " | Loss Count: " + lossCount + " | Guesses Left" + guessesLeft);
 
     document.getElementById("numGuesses").innerHTML = guessesLeft;
-    document.getElementById("wordToGuess").innerHTML = blankAndSuccesses.join (" ");
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join (" ");
     document.getElementById("wrongGuesses").innerHTML = wrongLetters.join (" ");
 
-    if (lettersinWord.toString() == blankAndSuccesses.toString()) {
+    if (lettersinWord.toString() == blanksAndSuccesses.toString()) {
         winCount++;
         alert("WINNER!!!");
 
@@ -114,6 +113,6 @@ document.onkeyup = function(event) {
    roundComplete();
 
    //Testing
-   alert(letterGuessed);
+   console.log(letterGuessed);
 
 }
